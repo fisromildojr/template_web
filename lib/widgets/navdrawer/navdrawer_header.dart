854fb956eda931e1980ca3dd6b5e094b5e82cad2
Romildo Jr <romildo.junior@trinove.com.br>
2023-10-16 14:47:11 -0300

    Backup

--- lib/widgets/navdrawer/navdrawer_header.dart
+++ lib/widgets/navdrawer/navdrawer_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:template_web/theme/my_colors.dart';
+import 'package:template_web/widgets/my_switch_theme.dart';
 
 class NavDrawerHeader extends StatelessWidget {
   const NavDrawerHeader({super.key});
@@ -7,27 +8,26 @@ class NavDrawerHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 150,
-        color: MyColors.primaryColor,
-        alignment: Alignment.center,
-        child: const Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'Flutter Template Web',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w800,
-                color: Colors.white,
-              ),
+      height: 150,
+      color: MyColors.primaryColor,
+      alignment: Alignment.center,
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Flutter Template Web',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w800,
+              color: Colors.white,
             ),
-            SizedBox(height: 20),
-            Text(
-              'by: @template_weboficial',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-              ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'by: @template_weboficial',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 12,
             ),
           ),
           SizedBox(height: 30),
